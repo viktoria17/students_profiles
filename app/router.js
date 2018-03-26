92bb6841c76d8e638f5e656ab8b69e6dd9ff9f0c
Viktoria Gužovskaja <viktoria.guzovskaja@gmail.com>
2018-03-26 08:18:37 +0300

    Get student data from the API and print it out on the user route

--- app/router.js
+++ app/router.js
@@ -1,3 +1,5 @@
+const Profile = require('./profile');
+
 // Handle HTTP route GET / and POST / i.e. home
 const home = (req, res) => {
 	if (req.url === '/') {
@@ -17,8 +19,26 @@ const user = (req, res) => {
 		res.statusCode = 200;
 		res.setHeader('Content-Type', 'text/plain');
 		res.write('Header\n');
-		res.write(`${username}\n`);
-		res.end('Footer\n');
+
+		// get json from Treehouse
+		const studentProfile = new Profile(username);
+
+		studentProfile.on('end', (profileJSON) => {
+			const values = {
+				username: profileJSON.profile_name,
+				avatar: profileJSON.gravatar_url,
+				badges: profileJSON.badges.length,
+				points: profileJSON.points.JavaScript
+			};
+
+			res.write(values.username + ' has ' + values.badges + ' badges and total ' + values.points + ' points in JavaScript\n');
+			res.end('Footer\n');
+		});
+
+		studentProfile.on('error', (err) => {
+			res.write(err.message);
+			res.end('Footer\n');
+		});
 	}
 };
 
