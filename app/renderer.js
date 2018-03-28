0d2bb68566b966222f66b7490557748da5a64262
Viktoria Gužovskaja <viktoria.guzovskaja@gmail.com>
2018-03-28 07:30:01 +0300

    Replace placeholders with actual values

--- app/renderer.js
+++ app/renderer.js
@@ -1,9 +1,22 @@
 const fs = require('fs');
 
+function mergeContent(values, content) {
+	for (var key in values) {
+		content = content.replace('{{' + key + "}}", values[key]);
+	}
+
+	return content;
+}
+
 const view = (templateName, values, res) => {
 	// Read from the template file
-	const fileContents = fs.readFileSync('../views/' + templateName + '.html');
+	// If the encoding option is specified then readFileSync func returns a string. Otherwise it returns a buffer.
+	let fileContents = fs.readFileSync('../views/' + templateName + '.html', {encoding: 'utf8'});
 
+	// Insert values in to the content
+	fileContents = mergeContent(values, fileContents);
+
+	// Write out the contents to the response
 	res.write(fileContents);
 };
 
